8e7848cd76a9c18d428ee970d7d8911f1d5729fc
Felipe Coury <felipe.coury@gmail.com>
2023-03-24 19:56:18 -0300

    feat: email details actions

--- client/src/components/Email/Actions.jsx
+++ client/src/components/Email/Actions.jsx
@@ -1,22 +1,60 @@
+import useEmailActions from "../../hooks/useEmailActions";
 import { useAppState } from "../../state/AppState";
 import "./Actions.css";
 
 export default function Actions() {
-  const { state } = useAppState();
+  const { state, dispatch } = useAppState();
+  const { archiveEmail, markAsSpam } = useEmailActions();
+
+  const handleArchiveClick = () => {
+    const id = state.email?.id;
+    dispatch({ type: "addEmailLoading", payload: id });
+    archiveEmail(id).finally(() => {
+      dispatch({ type: "nextEmail" });
+      dispatch({ type: "removeEmailLoading", payload: id });
+    });
+  };
+
+  const handleSpamClick = () => {
+    const id = state.email?.id;
+    dispatch({ type: "addEmailLoading", payload: id });
+    markAsSpam(id).finally(() => {
+      dispatch({ type: "nextEmail" });
+      dispatch({ type: "removeEmailLoading", payload: id });
+    });
+  };
+
+  const handlePreviousClick = () => {
+    dispatch({ type: "previousEmail" });
+  };
+
+  const handleNextClick = () => {
+    dispatch({ type: "nextEmail" });
+  };
+
+  const prevCss = state.currentEmailIndex === 0 ? " disabled" : "";
+  const nextCss =
+    state.currentEmailIndex === state.totalEmails - 1 ? " disabled" : "";
 
   return (
     <div className="actions">
       <div className="left">
         <i className="far fa-arrow-left"></i>
-        <i className="far fa-archive"></i>
-        <i className="far fa-exclamation-square"></i>
+        <i className="far fa-archive" onClick={handleArchiveClick}></i>
+        <i className="far fa-exclamation-square" onClick={handleSpamClick}></i>
       </div>
       <div className="middle">
-        <i className="far fa-chevron-left"></i>
+        <i
+          className={`far fa-chevron-left${prevCss}`}
+          onClick={handlePreviousClick}
+        ></i>
         <div className="page">
           {state.currentEmailIndex + 1} of {state.totalEmails}
         </div>
-        <i className="far fa-chevron-right"></i>
+        <i
+          className={`far fa-chevron-right${nextCss}`}
+          onClick={handleNextClick}
+        ></i>
       </div>
       <div className="right">
         <i className="far fa-reply"></i>
