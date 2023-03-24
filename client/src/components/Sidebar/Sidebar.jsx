79989f02a0206e7dd7fa1cc36f3862eb13e555dc
Felipe Coury <felipe.coury@gmail.com>
2023-03-24 17:24:08 -0300

    style: revamp of dark mode settings

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -5,106 +5,27 @@ import styles from "./Sidebar.module.css";
 
 export default function Sidebar() {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    const storedDarkMode = localStorage.getItem("darkMode");
+    return storedDarkMode === "true";
+  });
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark-mode");
+    } else {
+      document.documentElement.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
   const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-    if (!darkMode) {
-      document.documentElement.style.setProperty(
-        "--color-background",
-        "#2c2c2e"
-      );
-      document.documentElement.style.setProperty(
-        "--color-border-section",
-        "#4a4a4e"
-      );
-      document.documentElement.style.setProperty("--color-icon", "#a9a9a9");
-      document.documentElement.style.setProperty(
-        "--color-icon-hover-background",
-        "#3a3a3c"
-      );
-      document.documentElement.style.setProperty(
-        "--color-icon-selected",
-        "#5887ff"
-      );
-      document.documentElement.style.setProperty(
-        "--color-icon-selected-background",
-        "#1c1c1e"
-      );
-      document.documentElement.style.setProperty(
-        "--color-icon-selected-border",
-        "4px solid #0a84ff"
-      );
-      document.documentElement.style.setProperty("--color-text", "#fff");
-      document.documentElement.style.setProperty(
-        "--color-text-title",
-        "#f5f5f5"
-      );
-      document.documentElement.style.setProperty(
-        "--color-text-secondary",
-        "#9e9e9e"
-      );
-      document.documentElement.style.setProperty(
-        "--color-text-tertiary",
-        "#6d6d6f"
-      );
-      document.documentElement.style.setProperty(
-        "--color-selected-background",
-        "#3a3a3c"
-      );
-      document.documentElement.style.setProperty(
-        "--color-hover-background",
-        "#434245"
-      );
-    } else {
-      document.documentElement.style.setProperty("--color-background", "#fff");
-      document.documentElement.style.setProperty(
-        "--color-border-section",
-        "#f3f3f3"
-      );
-      document.documentElement.style.setProperty("--color-icon", "#5f5f61");
-      document.documentElement.style.setProperty(
-        "--color-icon-hover-background",
-        "#f7f7f7"
-      );
-      document.documentElement.style.setProperty(
-        "--color-icon-selected",
-        "#4368ae"
-      );
-      document.documentElement.style.setProperty(
-        "--color-icon-selected-background",
-        "#eef8ff"
-      );
-      document.documentElement.style.setProperty(
-        "--color-icon-selected-border",
-        "4px solid #2d62c9"
-      );
-      document.documentElement.style.setProperty("--color-text", "#000");
-      document.documentElement.style.setProperty(
-        "--color-text-title",
-        "#202127"
-      );
-      document.documentElement.style.setProperty(
-        "--color-text-secondary",
-        "#606063"
-      );
-      document.documentElement.style.setProperty(
-        "--color-text-tertiary",
-        "#b3b6b7"
-      );
-      document.documentElement.style.setProperty(
-        "--color-selected-background",
-        "#f0f0f0"
-      );
-      document.documentElement.style.setProperty(
-        "--color-hover-background",
-        "#f7f7f7"
-      );
-    }
+    const newDarkMode = !darkMode;
+    setDarkMode(newDarkMode);
+    localStorage.setItem("darkMode", newDarkMode.toString());
   };
 
   const handleOutsideClick = (event) => {
