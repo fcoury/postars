15f4882a63bf3c4f0a417469d092bae3045d8f3d
Felipe Coury <felipe.coury@gmail.com>
2023-03-23 22:29:56 -0300

    feat: keyboard shortcuts

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,10 +1,38 @@
 import { useEffect } from "react";
+import useEmailActions from "../hooks/useEmailActions";
 import { useAppState } from "../state/AppState";
 import Home from "./Home";
 import Login from "./Login";
 
 function Main() {
   const { state, dispatch } = useAppState();
+  const { archiveEmail } = useEmailActions();
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      console.log("hit", event.key);
+      switch (event.key) {
+        case "k":
+        case "ArrowUp":
+          dispatch({ type: "previousEmail" });
+          break;
+        case "j":
+        case "ArrowDown":
+          dispatch({ type: "nextEmail" });
+          break;
+        case "e":
+          console.log("state", state);
+          archiveEmail(state.email.id);
+          break;
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [state.email, state.emails]);
 
   useEffect(() => {
     const msalAccount = JSON.parse(localStorage.getItem("msalAccount"));
